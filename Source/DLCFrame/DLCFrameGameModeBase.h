11cf5672f16cac7ac4991bff53a1b4412f07591a
mecanim <laborotory@mail.ru>
2021-11-20 16:45:39 +0300

    Initial framework added

---
+++ Source/DLCFrame/DLCFrameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DLCFrameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DLCFRAME_API ADLCFrameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
